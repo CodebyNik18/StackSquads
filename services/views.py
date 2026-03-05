eb234ad50708d90ed60f9ebafe281d2b0b5b5421
Nikhil <1837nik@gmail.com>
2026-03-05 21:10:23 +0530

    completed views and templates integration

--- services/views.py
+++ services/views.py
@@ -2,3 +2,26 @@ from django.shortcuts import render
 
 def services(request):
     return render(request=request, template_name='services.html')
+
+def googleadservice(request):
+    return render(request=request, template_name='google_ad_service.html')
+
+
+def publicservice(request):
+    return render(request=request, template_name='public_service.html')
+
+
+def seoservice(request):
+    return render(request=request, template_name='seo_service.html')
+
+
+def socialservice(request):
+    return render(request=request, template_name='social_service.html')
+
+
+def videoservice(request):
+    return render(request=request, template_name='video_service.html')
+
+
+def webservice(request):
+    return render(request=request, template_name='web_service.html')
