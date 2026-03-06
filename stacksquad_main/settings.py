894f3b3e0cd8537fc6ae2819e09a25a489d8cdb6
Nikhil <1837nik@gmail.com>
2026-03-06 13:08:48 +0530

    change settings

--- stacksquad_main/settings.py
+++ stacksquad_main/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
-    'stacksquad_main/static/',
+    BASE_DIR/'stacksquad_main/static/',
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
